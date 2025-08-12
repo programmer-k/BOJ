64ec428749f2e7af515a18e478b4334a23b9a7df
Jiun Jeong <39119459+programmer-k@users.noreply.github.com>
2025-08-12 11:05:30 +0900

    [Gold III] Title: 공주님의 정원, Time: 40 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -39,67 +39,26 @@ void Solve() {
 
   priority_queue<Interval> pq;
   for (const Interval& interval : arr) {
-    //cout << "input: ";
-    //cout << interval.from.first  << '.' << interval.from.second << " - ";
-    //cout << interval.to.first  << '.' << interval.to.second << '\n';
-    if (pq.empty()) {
-      pq.push(interval);
-      //cout << "push at empty" << endl;
+    if (!pq.empty() && pq.top().to < interval.from)
       continue;
-    }
 
-    if (pq.top().to < interval.from)
-      continue;
-    
-    while (!pq.empty() && pq.top().from >= interval.from) {
-      //cout << "pop by first while" << endl;
+    while (!pq.empty()) {
+      Interval top = pq.top();
       pq.pop();
+
+      if (!((!pq.empty() && interval.from <= pq.top().to) || (pq.empty() && interval.from <= make_pair(3, 1)))) {
+        pq.push(top);
+        break;
+      }
     }
-
-    if (!pq.empty()) {
-      bool push_last = true;
-      Interval last;
-      do {
-        last = pq.top();
-        //cout << "top: ";
-        //cout << pq.top().from.first  << '.' << pq.top().from.second << " - ";
-        //cout << pq.top().to.first  << '.' << pq.top().to.second << '\n';
-        pq.pop();
-
-        if (pq.empty() && interval.from <= make_pair(3, 1)) {
-          push_last = false;
-          break;
-        }
-
-        //cout << "pop" << endl;
-        //cout << "size: " << ssize(pq) << endl;
-      } while (!pq.empty() && pq.top().to >= interval.from);
-      
-      if (push_last)
-        pq.push(last);
-    }
-
 
     pq.push(interval);
-    //cout << "size: " << ssize(pq) << endl;
-    //cout << "last top: ";
-    //cout << pq.top().from.first  << '.' << pq.top().from.second << " - ";
-    //cout << pq.top().to.first  << '.' << pq.top().to.second << '\n';
-  }
-
-  //cout << endl;
-  if (pq.top().to.first < 12) {
-    cout << "0\n";
-    //cout << "123";
-    return;
   }
 
   int total = ssize(pq);
   int count1 = 0;
   int count2 = 0;
   while (!pq.empty()) {
-    //cout << pq.top().from.first  << '.' << pq.top().from.second << " - ";
-    //cout << pq.top().to.first  << '.' << pq.top().to.second << '\n';
     if (pq.top().to.first >= 12)
       ++count1;
 
@@ -108,25 +67,12 @@ void Solve() {
     pq.pop();
   }
 
-  if (count2 == 0) {
+  if (count1 == 0 || count2 == 0) {
     cout << "0\n";
-    //cout << "1234";
     return;
   }
 
-  //cout << "count1: " << count1 << endl;
-  //cout << "count2: " << count2 << endl;
   cout << total - (count1 - 1) - (count2 - 1) << endl;
-
-
-
-  //cout << pq.size() << '\n';
-
-  /*while (!pq.empty()) {
-    cout << pq.top().from.first  << '.' << pq.top().from.second << " - ";
-    cout << pq.top().to.first  << '.' << pq.top().to.second << '\n';
-    pq.pop();
-  }*/
 }
 
 int main() {
