838b87b33af376b48283224b6eef1880592b025a
Jiun Jeong <39119459+programmer-k@users.noreply.github.com>
2025-09-04 15:25:09 +0900

    [Gold III] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.cc
+++ 백준/Gold/1082. 방 번호/방 번호.cc
@@ -1,21 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
-
-struct Data {
-  string str;
-  
-  bool operator<(const Data& rhs) const {
-    bool all_zero = true;
-    for (int i = 0; i < ssize(str); ++i)
-      
-
-    if (str.size() != rhs.str.size())
-      return str.size() < rhs.str.size();
-    return str < rhs.str;
-  }
-};
 
 int n, m;
 vector<int> prices;
@@ -29,7 +15,7 @@ void GetInput() {
   prices.resize(n);
   for (int i = 0; i < n; ++i)
     cin >> prices[i];
-  
+
   cin >> m;
 }
 
@@ -39,7 +25,7 @@ bool Compare(const string& str1, const string& str2) {
   return str1 < str2;
 }
 
-void Solve1() {
+void Solve() {
   vector<string> dp(m + 1);
 
   for (int i = 0; i <= m; ++i)
@@ -51,44 +37,11 @@ void Solve1() {
   string answer;
   for (int i = 0; i <= m; ++i)
     answer = max(answer, dp[i], Compare);
-  //cout << dp[m] << '\n';
   cout << answer << '\n';
-}
-
-void Solve() {
-  vector<vector<Data>> dp(m + 1, vector<Data>(n));
-  for (int i = 0; i <= m; ++i) {
-    for (int j = 0; j < n; ++j) {
-      if (i - prices[j] >= 0) {
-        if (dp[i - prices[j]][j].str == to_string(0)) {
-          dp[i][j].str = to_string(0);
-        } else {
-          for (int k = 0; k < n; ++k)
-            if (i - prices[k] >= 0)
-            for (int l = 0; l < n; ++l)
-              dp[i][j] = max(dp[i][j], Data(dp[i - prices[k]][l].str + to_string(k)));
-        }
-      }
-    }
-  }
-
-  for (int i = 0; i <= m; ++i) {
-    cout << i << ": ";
-    for (int j = 0; j < n; ++j) {
-      cout << dp[i][j].str << ' ';
-    }
-    cout << '\n';
-  }
-
-  Data answer;
-  for (int i = 0; i < n; ++i)
-    answer = max(answer, dp[m][i]);
-  cout << answer.str << '\n';
 }
 
 int main() {
   GetInput();
-  //Solve();
-  Solve1();
+  Solve();
   return 0;
 }
