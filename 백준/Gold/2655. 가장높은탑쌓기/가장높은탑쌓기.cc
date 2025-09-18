3f41029d87ee64f85004c4d5e9abcec635ef8f76
Jiun Jeong <39119459+programmer-k@users.noreply.github.com>
2025-09-19 00:21:58 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.cc
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+struct Brick {
+  int area;
+  int height;
+  int weight;
+  int index;
+
+  bool operator<(const Brick& rhs) const {
+    return area < rhs.area;
+  }
+
+  bool operator>(const Brick& rhs) const {
+    return rhs < *this;
+  }
+};
+
+int n;
+vector<Brick> bricks;
+
+void GetInput() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    int area, height, weight;
+    cin >> area >> height >> weight;
+    bricks.push_back({area, height, weight, i + 1});
+  }
+}
+
+void Solve() {
+  sort(bricks.begin(), bricks.end(), greater<Brick>());
+
+  vector<pair<int, int>> dp(n);
+  for (int i = 0; i < n; ++i) {
+    dp[i].first = bricks[i].height;
+    dp[i].second = -1;
+    for (int j = 0; j < i; ++j) {
+      if (bricks[i].weight < bricks[j].weight &&
+          dp[i].first < dp[j].first + bricks[i].height) {
+        dp[i].first = dp[j].first + bricks[i].height;
+        dp[i].second = j;
+      }
+    }
+  }
+
+  int count = 0;
+  vector<int> answers;
+  int index = max_element(dp.begin(), dp.end()) - dp.begin();
+  while (index != -1) {
+    answers.push_back(bricks[index].index);
+    ++count;
+    index = dp[index].second;
+  }
+
+  cout << count << '\n';
+  for (int answer : answers)
+    cout << answer << '\n';
+}
+
+int main() {
+  GetInput();
+  Solve();
+  return 0;
+}
